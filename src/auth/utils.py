d8666cfbfa79aacbbea616277bddb02442719665
Or Hasson <hassonor@gmail.com>
2025-01-04 17:13:45 +0200

    adding .env.example + decode and encode jwt token

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,6 +1,12 @@
+from datetime import timedelta, datetime
 from passlib.context import CryptContext
+import jwt
+import uuid
+import logging
+from src.config import Config
 
 passwd_context = CryptContext(schemes=["bcrypt"])
+ACCESS_TOKEN_EXPIRY = 3600
 
 
 def generate_passwd_hash(password: str) -> str:
@@ -10,3 +16,30 @@ def generate_passwd_hash(password: str) -> str:
 
 def verify_password(password: str, hashed_password: str) -> bool:
     return passwd_context.verify(password, hashed_password)
+
+
+def create_access_token(user_data: dict, expiry: timedelta = None, refresh: bool = False) -> str:
+    payload = {}
+    payload['user'] = user_data
+    payload['exp'] = datetime.now() + (expiry if expiry is not None else timedelta(seconds=ACCESS_TOKEN_EXPIRY))
+    payload['jti'] = str(uuid.uuid4())
+    payload['refresh'] = refresh
+
+    token = jwt.encode(payload=payload, key=Config.JWT_SECRET, algorithm=Config.JWT_ALGORITHM)
+
+    return token
+
+
+def decode_token(token: str) -> dict:
+    try:
+        token_data = jwt.decode(
+            jwt=token,
+            key=Config.JWT_SECRET,
+            algorithms=[Config.JWT_ALGORITHM]
+        )
+
+        return token_data
+
+    except jwt.PyJWT as e:
+        logging.exception(e)
+        raise None
