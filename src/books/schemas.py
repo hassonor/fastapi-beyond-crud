f4cfbe5d27d95f6d7d4259a1ea6348b1b8cac714
Or Hasson <hassonor@gmail.com>
2025-01-11 15:11:57 +0200

    add tags feature + add more reviews routes

--- src/books/schemas.py
+++ src/books/schemas.py
@@ -3,6 +3,7 @@ import uuid
 from pydantic import BaseModel
 from datetime import datetime, date
 from src.reviews.schemas import ReviewModel
+from src.tags.schemas import TagModel
 
 
 class Book(BaseModel):
@@ -19,6 +20,7 @@ class Book(BaseModel):
 
 class BookDetailModel(Book):
     reviews: List[ReviewModel]
+    tags: List[TagModel]
 
 
 class BookCreateModel(BaseModel):
--- src/reviews/service.py
+++ src/reviews/service.py
@@ -2,6 +2,7 @@ import logging
 
 from fastapi.exceptions import HTTPException
 from fastapi import status
+from sqlmodel import desc, select
 from sqlmodel.ext.asyncio.session import AsyncSession
 from .schemas import ReviewCreateModel
 from src.auth.service import UserService
@@ -13,23 +14,30 @@ user_service = UserService()
 
 
 class ReviewService:
-    async def add_review_to_book(self, user_email: str, book_uid: str, review_data: ReviewCreateModel,
-                                 session: AsyncSession):
+    async def add_review_to_book(
+            self,
+            user_email: str,
+            book_uid: str,
+            review_data: ReviewCreateModel,
+            session: AsyncSession,
+    ):
         try:
             book = await book_service.get_book_by_id(book_uid=book_uid, session=session)
-            user = await user_service.get_user_by_email(email=user_email, session=session)
-
+            user = await user_service.get_user_by_email(
+                email=user_email, session=session
+            )
             review_data_dict = review_data.model_dump()
-            new_review = Review(**review_data_dict)
-
             if not book:
-                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Book not found')
+                raise HTTPException(
+                    detail="Book not found", status_code=status.HTTP_404_NOT_FOUND
+                )
 
             if not user:
-                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
+                raise HTTPException(
+                    detail="User not found", status_code=status.HTTP_404_NOT_FOUND
+                )
 
-            new_review.user = user
-            new_review.book = book
+            new_review = Review(**review_data_dict, user=user, book=book)
 
             session.add(new_review)
 
@@ -40,6 +48,37 @@ class ReviewService:
         except Exception as e:
             logging.exception(e)
             raise HTTPException(
+                detail="Oops... something went wrong!",
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail="Oops.... Something went wrong"
             )
+
+    async def get_review(self, review_uid: str, session: AsyncSession):
+        statement = select(Review).where(Review.uid == review_uid)
+
+        result = await session.exec(statement)
+
+        return result.first()
+
+    async def get_all_reviews(self, session: AsyncSession):
+        statement = select(Review).order_by(desc(Review.created_at))
+
+        result = await session.exec(statement)
+
+        return result.all()
+
+    async def delete_review_to_from_book(
+            self, review_uid: str, user_email: str, session: AsyncSession
+    ):
+        user = await user_service.get_user_by_email(user_email, session)
+
+        review = await self.get_review(review_uid, session)
+
+        if not review or (review.user != user):
+            raise HTTPException(
+                detail="Cannot delete this review",
+                status_code=status.HTTP_403_FORBIDDEN,
+            )
+
+        session.delete(review)
+
+        await session.commit()
